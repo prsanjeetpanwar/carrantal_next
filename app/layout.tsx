7154b971eed781a02d2710435ece82420e2ab694
prsanjeet panwar <prsanjeetpanwar729@gmail.com>
2023-06-27 15:11:55 +0530

    add header and footer with responsivenase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import { Footer, Navbar } from './components'
 import './globals.css'
+import Navbar from '@/app/components/Navbar'
+import Footer from '@/app/components/Footer'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
-
 export const metadata = {
   title: 'Car Town',
   description: 'this is best place to get your dream car',
